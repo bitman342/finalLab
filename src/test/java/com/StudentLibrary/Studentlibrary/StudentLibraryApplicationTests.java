c53b07ac6d55fba8264935aef39d3cbf02d49926
mobe4 <musebirhanu.teshale@miu.edu>
2024-04-26 20:42:09 -0500

    aa

--- src/test/java/com/StudentLibrary/Studentlibrary/StudentLibraryApplicationTests.java
+++ src/test/java/com/StudentLibrary/Studentlibrary/StudentLibraryApplicationTests.java
@@ -3,7 +3,7 @@ package com.StudentLibrary.Studentlibrary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StudentLibraryApplicationTests {
 
 	@Test
